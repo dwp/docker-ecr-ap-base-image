9e4d766cea42d5eb3799ecde28f21c8ebca486bb
WW Teo <whay-wang.teo@engineering.digital.dwp.gov.uk>
2021-09-29 16:25:05 +0100

    changes with sed and github workflow

--- ecr.tf
+++ ecr.tf
@@ -1,16 +1,16 @@
-resource "aws_ecr_repository" "docker-ecr-template" {
-  name = "docker-ecr-template"
+resource "aws_ecr_repository" "docker-ecr-ap-base-image" {
+  name = "docker-ecr-ap-base-image"
   tags = merge(
     local.common_tags,
-    { DockerHub : "dwpdigital/docker-ecr-template" }
+    { DockerHub : "dwpdigital/docker-ecr-ap-base-image" }
   )
 }
 
-resource "aws_ecr_repository_policy" "docker-ecr-template" {
-  repository = aws_ecr_repository.docker-ecr-template.name
+resource "aws_ecr_repository_policy" "docker-ecr-ap-base-image" {
+  repository = aws_ecr_repository.docker-ecr-ap-base-image.name
   policy     = data.terraform_remote_state.management.outputs.ecr_iam_policy_document
 }
 
-output "ecr_example_url" {
-  value = aws_ecr_repository.docker-ecr-template.repository_url
+output "ecr_docker-ecr-ap-base-image_url" {
+  value = aws_ecr_repository.docker-ecr-ap-base-image.repository_url
 }
